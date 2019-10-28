cf82e2cfccdb060241fc17aea32d94fb464627f4
unknown <emilia_nedialkova@abv.bg>
2019-10-29 00:01:31 +0200

    added first exe

---
+++ 01. First Steps In Coding/src/P_01_Hello_SoftUni.java
@@ -0,0 +1,6 @@
+public class P_01_Hello_SoftUni {
+    public static void main(String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+}
+
---
+++ 01. First Steps In Coding/src/P_03_Nums_1_To_10.java
@@ -0,0 +1,7 @@
+public class P_03_Nums_1_To_10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
